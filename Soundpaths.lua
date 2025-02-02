2636cf5edd1b0eda886a6d96be5b8bc2d7266523
Ondrej Slejtr <o.slejtr@gmail.com>
2025-02-02 17:55:00 +0100

    feat: initialize project

---
+++ Soundpaths.lua
@@ -0,0 +1,23 @@
+local LSM = LibStub("LibSharedMedia-3.0")
+
+-- -----
+-- BACKGROUND
+-- -----
+
+-- -----
+--  BORDER
+-- ----
+
+-- -----
+--   FONT
+-- -----
+
+-- -----
+--   SOUND
+-- -----
+LSM:Register("sound", "|c34eb7100Spred Zmrdi|r", [[Interface\Addons\SharedMedia_ZenGarden\sound\spredzmrdi.ogg]])
+LSM:Register("sound", "|c34eb7100Brainfart|r", [[Interface\Addons\SharedMedia_ZenGarden\sound\brainfart.ogg]])
+
+-- -----
+--   STATUSBAR
+-- -----
